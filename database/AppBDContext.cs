c62007384d8035aa8c45c5ac75408b3d083a12da
Angell-26 <juegosps3rd@gmail.com>
2026-04-07 22:23:00 -0400

    Add files via upload

---
+++ database/AppBDContext.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using GestorGastos.Models;
+
+namespace GestorGastos.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            //Uso de SQLIte como base de datos
+           
+            optionsBuilder.UseSqlite("Data Source=gastos.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Algunas Configuraciones
+            modelBuilder.Entity<Expense>()
+                .HasOne(e => e.Category)
+                .WithMany(c => c.Expenses)
+                .HasForeignKey(e => e.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict); // Esto Evita borrar categorías con gastos
+
+            // Datos de categorías
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Alimentación" },
+                new Category { Id = 2, Name = "Transporte" },
+                new Category { Id = 3, Name = "Entretenimiento" },
+                new Category { Id = 4, Name = "Servicios" },
+                new Category { Id = 5, Name = "Otros" }
+            );
+        }
+    }
+
+}
+
